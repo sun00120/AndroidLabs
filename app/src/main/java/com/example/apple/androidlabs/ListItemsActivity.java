a7810850e61e05a359eabc8c842d3824804e1b2a
apple <sun00120@algonquinlive.com>
2018-09-14 13:29:04 -0400

    Added 3 activies for Lab 2.

--- app/src/main/java/com/example/apple/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/apple/androidlabs/ListItemsActivity.java
@@ -1,9 +1,10 @@
 package com.example.apple.androidlabs;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ListItemsActivity extends AppCompatActivity {
+public class ListItemsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
